e3a83368de3ff8860d3faa03da725854215193a9
Wu Jiahao <wujiahao00@gmail.com>
2021-10-31 11:59:52 +0800

    env varaibles

--- locallibary/settings.py
+++ locallibary/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-psb_#pwsr5n8z%
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerful-taiga-10343.herokuapp.com','127.0.0.1']
+# For example:
+# ALLOWED_HOSTS = ['fathomless-scrubland-30645.herokuapp.com', '127.0.0.1']
+
 
 
 # Application definition
